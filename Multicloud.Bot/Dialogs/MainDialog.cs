d2f7baa55f3208a42b567b920a592fbd79ca9d73
Jagdish Kumawat <jagdish.kumawat@dewiride.com>
2023-03-14 22:33:26 +0530

    remove bot workspace

--- Multicloud.Bot/Dialogs/MainDialog.cs
+++ Multicloud.Bot/Dialogs/MainDialog.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Bot;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
